6ca1954f729f5d7c3dc304a4cab92bdfd5388154
Don Park <donp@donp.org>
2015-09-11 17:05:05 -0700

    pull down census place data

--- tiger-json.sh
+++ tiger-json.sh
@@ -1,3 +1,4 @@
+echo Converting Tiger Place Data
 mkdir -p json
 for full in ftp2.census.gov/geo/tiger/TIGER2014/PLACE/*zip
 do
@@ -9,3 +10,11 @@ ogr2ogr -f geoJSON json/$name.json jail/$name.shp
 ls -l json/$name.json
 rm -rf jail
 done
+
+echo Unpacking Place Census
+mkdir -p Place_2010Census
+cd Place_2010Census
+unzip ../Place_2010Census_DP1.zip
+echo Converting Place Census
+ogr2ogr -f geoJSON Place_2010Census_DP1.json Place_2010Census_DP1.shp
+
--- tiger-pull.sh
+++ tiger-pull.sh
@@ -1,1 +1,3 @@
 wget --mirror ftp://ftp2.census.gov/geo/tiger/TIGER2014/PLACE/
+wget http://www2.census.gov/geo/tiger/TIGER2010DP1/Place_2010Census_DP1.zip
+
